70def901cc9b579d4feee98053909a650658359c
sunnatmaxkamboyev-source <sunnatmaxkamboyev@gmail.com>
2026-04-02 17:20:45 +0500

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css";
+
+function App(){
+    return(
+        <div className='app'>
+            <h1>Movie Search App</h1>
+        </div>
+    )
+}
+
+export default App;
